be5e4af131c5375610913c6657381ca3e848eb1d
AmberAAA <anborong@outlook.com>
2024-12-12 10:19:33 +0800

    monorepo & workspace

---
+++ packages/umi-ts-antd-react/src/pages/add.ts
@@ -0,0 +1,7 @@
+/**
+ * @param a {number} A
+ **/
+export const add = (a: number, b: number) => {
+  console.log("Amber");
+  return a + b;
+};
--- packages/umi-ts-antd-react/src/pages/index.tsx
+++ packages/umi-ts-antd-react/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { App, Button } from "antd";
+import { add } from "./add";
 
 export default function HomePage() {
   const { modal } = App.useApp();
@@ -7,6 +8,7 @@ export default function HomePage() {
       content: "你确定?",
     });
   };
+  console.log(add(1, 3));
   return (
     <div>
       <Button onClick={open}>弹窗国际化</Button>
