82ca1fd4763c914e2c60c8073302cf9837486ce0
AmberAAA <anborong@outlook.com>
2024-12-24 15:00:34 +0800

    add nextjs

--- packages/umi-ts-antd-react/src/pages/add.test.ts
+++ packages/umi-ts-antd-react/src/pages/add.test.ts
@@ -1,5 +1,9 @@
-import { add } from './add'
+import { add, sub } from './add'
 
 test('A', () => {
     expect(add(1,2)).toBe(3)
 })
+
+test('B', () => {
+    expect(sub(2,2)).toBe(0)
+})  
--- packages/umi-ts-antd-react/src/pages/add.ts
+++ packages/umi-ts-antd-react/src/pages/add.ts
@@ -6,3 +6,11 @@
 export const add = (a: number, b: number) => {
   return a + b;
 };
+
+/**
+ * 
+ * @param a 
+ * @param b 
+ * @returns 
+ */
+export const sub = (a: number, b: number) => a - b;
